997acc4e60899dce639350928eb64a18db492c99
dylenthomas <dliverman621@gmail.com>
2024-11-17 11:16:37 -0500

    project now builds with cmake

--- listener.cpp
+++ listener.cpp
@@ -1,43 +1,58 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <string>
+#include <iostream>
+#include <memory>
+#include <chrono>
+#include <thread>
 #include <cstring>
 
+#include <torch/script.h>
 #include <portaudio.h>
-#include <torch/torch.h>
 
 #define SAMPLERATE 44100
-#define FRAMESPERBUFFER 44100
+#define FRAMESPERBUFFER 44100 //make each buffer hold 1 second of data for prediction
 
-using namespace std;
-
-//https://www.youtube.com/watch?v=jpsJCji71Ec
+//https://pytorch.org/tutorials/advanced/cpp_export.html
+//https://github.com/pytorch/examples/tree/main/cpp
 
 static void checkErr(PaError err) {
     if (err != paNoError) {
-        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
-        exit(EXIT_FAILURE);
+        std::cout << "PortAudio error: " << err << std::endl;
+        exit(0);
     }
 }
 
-int getMicrophone(string name) {
-    //Returns the index of the first microphone it finds that contains the name, if no microphone exists then returns -1
-
-    //Init vars
+static void printAudioDevices() {
     int numDevices = Pa_GetDeviceCount();
     const PaDeviceInfo* device;
-    string deviceName;
+    std::string deviceName;
+    int outputChannels;
+    int inputChannels;
+
+    for (int i=0; i<numDevices; i++) {
+        device = Pa_GetDeviceInfo(i);
+        deviceName = device->name;
+        outputChannels = device->maxOutputChannels;
+        inputChannels = device->maxInputChannels;
+
+        std::cout << "Device [" << deviceName << "] has: " << outputChannels << "output channels, and " << inputChannels << "input channels\n";
+    }
+}
+
+static int getMicrophone(std::string name) {
+    int numDevices = Pa_GetDeviceCount();
+    const PaDeviceInfo* device;
+    std::string deviceName;
     int outputChannels;
     int micIndex = -1;
 
-    //Find the device index
+    //find the device index
     for (int i=0; i<numDevices; i++) {
         device = Pa_GetDeviceInfo(i);
         deviceName = device->name;
         outputChannels = device->maxOutputChannels;
 
-        //Check if deviceName contains name
-        int x = 0;
+        //check if deviceName contains name
+        unsigned long int x = 0;
         while (true) {
             if (deviceName[x] == name[x] && x <= name.length() && outputChannels == 0) {
                 x++;
@@ -50,15 +65,20 @@ int getMicrophone(string name) {
     return micIndex;
 }
 
-class listener {
+class Listener {
     private:
         PaStreamParameters inputParameters;
         PaError err;
-        PaStream* stream;
+        PaStream* stream1;
+        PaStream* stream2;
+
+        torch::jit::script::Module model;
+        std::vector<torch::jit::IValue> modelInput;
+        std::vector<torch::jit::IValue> lastInput;
 
     public:
-        void setInputParams(int micIndex){
-            memset(&inputParameters, 0, sizeof(inputParameters));
+        void setInputParams(int micIndex) {
+            memset(&inputParameters, 0, sizeof(inputParameters)); //sets sizeof(inputParameters) number of values at &inputParameters to 0
             inputParameters.channelCount = 1;
             inputParameters.device = micIndex;
             inputParameters.hostApiSpecificStreamInfo = NULL;
@@ -66,9 +86,21 @@ class listener {
             inputParameters.suggestedLatency = Pa_GetDeviceInfo(micIndex)->defaultLowInputLatency;
         }
 
-        void openStream(){
+        void openStreams() {
             err = Pa_OpenStream(
-                    &stream,
+                    &stream1,
+                    &inputParameters,
+                    NULL,
+                    SAMPLERATE,
+                    FRAMESPERBUFFER,
+                    paNoFlag,
+                    callback,
+                    NULL
+                    );
+            checkErr(err);
+
+            err = Pa_OpenStream(
+                    &stream2,
                     &inputParameters,
                     NULL,
                     SAMPLERATE,
@@ -80,16 +112,53 @@ class listener {
             checkErr(err);
         }
 
-        void startStream(){
-            err = Pa_StartStream(&stream);
+        void startStreams() {
+            auto start = std::chrono::high_resolution_clock::now();
+            err = Pa_StartStream(&stream1);
+            checkErr(err);
+            auto end = std::chrono::high_resolution_clock::now();
+
+            //create two audio streams 90 deg out of phase
+            std::chrono::milliseconds offset(500);
+            const std::chrono::duration<double> wait = offset - (end - start);
+            std::cout << "Time to start stream 1: " << wait.count() << std::endl;
+            std::this_thread::sleep_for(wait);
+
+            err = Pa_StartStream(&stream2);
             checkErr(err);
         }
 
-        void stopStream(){
-            err = Pa_StopStream(&stream);
+        void stopStreams() {
+            err = Pa_StopStream(&stream1);
+            checkErr(err);
+
+            err = Pa_StopStream(&stream2);
             checkErr(err);
         }
 
+        void loadModel() {
+            try {
+                model = torch::jit::load("/home/dylenthomas/Documents/whisper/.model/whisper.pt");
+            } catch (const c10::Error& e) {
+                std::cerr << "The model could not be loaded:\n" << e.what();
+                exit(0);
+            }
+            std::cout << "Model loaded successfuly\n";
+        }
+
+        int predict() {
+            if (modelInput != lastInput){
+                {
+                    torch::NoGradGuard no_grad; //disable grad
+                    torch::Tensor output = model.forward(modelInput).toTensor();
+                    return output[0].argmax(-1).item<int>();
+                }
+
+                lastInput = modelInput;
+            } else {
+                return -1;
+            }
+        }
 
     private:
         static int callback(
@@ -99,7 +168,7 @@ class listener {
                 const PaStreamCallbackTimeInfo* timeInfo,
                 PaStreamCallbackFlags statusFlags,
                 void* userData
-                ){
+                ) {
             int* input = (int*)inputBuffer;
             (void)outputBuffer; //prevent unused var error
 
@@ -108,34 +177,37 @@ class listener {
 };
 
 int main() {
-    //Start up
+    //init portaudio
     PaError err;
     err = Pa_Initialize();
     checkErr(err);
 
-    //Find the microphone index
-    const string name = "iPhone";
+    //find mic index
+    const std::string name = "Blue Snowball";
     int micIndex = getMicrophone(name);
 
     if (micIndex < 0) {
-        printf("The device: %s was not found.\n", name.c_str());
-        return EXIT_FAILURE;
-    } else {
-        printf("Device (%s) found at index: %d\n", name.c_str(), micIndex);
+        std::cout << "The device [" << name << "] could not be found.\n";
+        std::cout << "This is a list of all the devices:\n";
+        printAudioDevices();
+        return 0;
     }
+    std::cout << "The device [" << name << "] was found at index: " << micIndex << std::endl;
+
+    Listener listener;
+    listener.setInputParams(micIndex);
+    listener.loadModel();
+
+    listener.openStreams();
+    listener.startStreams();
 
 
 
 
+    //shut down
+    listener.stopStreams();
 
-
-
-
-
-
-    //Shut down
     err = Pa_Terminate();
     checkErr(err);
-
-    return EXIT_SUCCESS;
+    return 0;
 }
