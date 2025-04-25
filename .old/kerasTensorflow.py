c97bef81715190c081f62dac10c8f762bf8090aa
dylenthomas <dliverman621@gmail.com>
2025-04-25 00:00:21 -0400

    start with transformer asr tutorial

---
+++ .old/kerasTensorflow.py
@@ -0,0 +1,101 @@
+import re
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import tensorflow as tf
+import keras
+from keras import layers
+
+pattern_wav_name = re.compile(r'[^/\\\.]+)')
+
+class TokenEmbedding(layers.Layer):
+    def __init__(self, num_vocab=1000, maxlen=100, num_hid=64):
+        super().__init__()
+        self.emb = layers.Embedding(num_vocab, num_hid)
+        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=num_hid)
+
+    def call(self, x):
+        maxlen = tf.shape(x)[-1]
+        x = self.emb(x)
+        positions = tf.range(start=0, limit=maxlen, delta=1)
+        positions = self.pos_emb(positions)
+
+        return x + positions
+    
+class SpeechFeatureEmbedding(layers.Layer):
+    def __init__(self, num_hid=64, maxlen=100):
+        super().__init__()
+        self.conv1 = layers.Conv1D(num_hid, 11, strides=2, padding="same", activation="relu")
+        self.conv2 = layers.Conv1D(num_hid, 11, strides=2, padding="same", activation="relu")
+        self.conv3 = layers.Conv1D(num_hid, 11, strides=2, padding="same", activation="relu")
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        
+        return x
+    
+class TransformerEncoder(layers.Layer):
+    def __init__(self, embed_dim, num_heads, feed_forward_dim, rate=0.1):
+        super().__init__()
+        self.att = layers.MultiHeadAttention(num_heads, key_dim=embed_dim)
+        self.ffn = keras.Sequential([
+            layers.Dense(feed_forward_dim, activation="relu"),
+            layers.Dense(embed_dim),
+        ])
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epislon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
+
+    def call(self, inputs, training=False):
+        attn_output = self.attn(inputs, inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(out1 + ffn_output)  
+        
+        return out2
+
+class TransformerDecoder(layers.Layer):
+    def __init__(self, embed_dim, num_heads, feed_forward_dim, rate=0.1):
+        super().__init__()
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6) 
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6) 
+        self.layernorm3 = layers.LayerNormalization(epsilon=1e-6) 
+        self.self_att = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.enc_att = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.self_droppout = layers.Dropout(0.5)
+        self.enc_dropout = layers.Dropout(0.1)
+        self.ffn_dropout = layers.Dropout(0.1)
+        self.ffn = keras.Sequential([
+            layers.Dense(feed_forward_dim, activation="relu"),
+            layers.Dense(embed_dim),
+        ])
+
+    def casual_attention_mask(self, batch_size, n_dset, n_src, dtype):
+        i = tf.range(n_dset)[:, None]
+        j = tf.range(n_src)
+        m = i >= j - n_src + n_dset
+        mask = tf.cast(m, dtype)
+        mask = tf.reshape(mask, [1, n_dset, n_src])
+        mult = tf.concat([tf.expand_dims(batch_size, -1), tf.constant([1, 1], dtype=tf.int32)], 0)
+
+        return tf.title(mask, mult)
+    
+    def call(self, enc_out, target):
+        input_shape = tf.shape(target)
+        batch_size = input_shape[0]
+        seq_len = input_shape[1]
+        causal_mask = self.causal_attention_mask(batch_size, seq_len, seq_len, tf.bool)
+        target_att = self.set_att(target, target, attention_mask=causal_mask)
+        target_norm = self.layernorm1(target + self.self_droppout(target_att))
+        enc_out = self.enc_att(target_norm, enc_out)
+        enc_out_norm = self.layernorm2(self.enc_dropout(enc_out) + target_norm)
+        ffn_out = self.ffn(enc_out_norm)
+        ffn_out_norm = self.layernorm3(enc_out_norm, self.ffn_dropout(ffn_out))
+
+        return ffn_out_norm
--- .old/wav2vec2Test.py
+++ .old/wav2vec2Test.py
@@ -1,6 +1,9 @@
+import ai_edge_torch
+import numpy as np
 import torch
 import torchaudio
-import matplotlib.pyplot as plt
+import torchvision
+import os
 
 class GreedyCTCDecoder(torch.nn.Module):
     def __init__(self, labels, blank=0):
@@ -14,26 +17,48 @@ class GreedyCTCDecoder(torch.nn.Module):
         indices = [i for i in indices if i != self.blank]
         return "".join([self.labels[i] for i in indices])
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+model_downloaded = os.path.exists("/home/dylenthomas/whisper/.model/Wav2Vec2.pt")
+#device = "cuda" if torch.cuda.is_available() else "cpu"
 
-bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
-model = bundle.get_model().to(device)
+if model_downloaded:
+    bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
+    model = bundle.get_model().eval()
+else:
+    bundle = torchaudio.pipelines.WAV2VEC2_ASR_BASE_960H
+    model = torch.jit.load(".model/Wav2Vec2.pt").eval()
 
-waveform, sample_rate = torchaudio.load("test.wav")
-waveform = waveform.to(device)
+#model = torchvision.models.resnet18(torchvision.models.ResNet18_Weights.IMAGENET1K_V1).eval()
+#sample_inputs = (torch.randn(1, 3, 224, 224),)
+#torch_outputs = model(*sample_inputs)
 
-if sample_rate != bundle.sample_rate:
-    waveform = torchaudio.functional.resample(waveform, sample_rate, bundle.sample_rate)
+model = model.to(device="cpu")
 
-with torch.inference_mode():
-    emission, _ = model(waveform)
+#waveform, sample_rate = torchaudio.load("test.wav")
+sample_inputs = (torch.rand(1, 16000),)
+torch_outputs = model(*sample_inputs)
 
-decoder = GreedyCTCDecoder(labels=bundle.get_labels())
-print(bundle.get_labels())
-print(type(bundle.get_labels()))
-transcript = decoder(emission[0])
-print(transcript)
+#if sample_rate != bundle.sample_rate:
+#    waveform = torchaudio.functional.resample(waveform, sample_rate, bundle.sample_rate)
 
-print("saving the model...")
-model_scripted = torch.jit.script(model)
-model_scripted.save(".model/Wav2Vec2.pt")
+#with torch.inference_mode():
+#    torch_output, _ = model(waveform)
+
+#decoder = GreedyCTCDecoder(labels=bundle.get_labels())
+#transcript = decoder(torch_output[0])
+
+#if model_downloaded:
+#    print("saving the model...")
+#    model_scripted = torch.jit.script(model)
+#    model_scripted.save(".model/Wav2Vec2.pt")
+
+# convert model to tflite
+edge_model = ai_edge_torch.convert(model, sample_inputs)
+edge_output = edge_model(*sample_inputs)
+
+# validate the model
+if np.allclose(torch_outputs.detach().numpy(), edge_output, atol=1e-5, rtol=1e-5):
+    print("Inference result with PyTorch and TfLite was within tolerance")
+else:
+    print("Something wrong with PyTorch --> TfLite")
+
+edge_model.export("/home/dylenthomas/whisper/.model/Wav2Vec2.tflite")
