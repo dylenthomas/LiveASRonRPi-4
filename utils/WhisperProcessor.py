d45080f585780bcec2111aacb719c84a3eea8eb0
dylenthomas <dliverman621@gmail.com>
2025-05-14 14:24:33 -0400

    going to use whisper from openai for ASR, started by recreating a feature extractor from HF

---
+++ utils/WhisperProcessor.py
@@ -0,0 +1,91 @@
+import json
+import torchaudio.transforms as T
+import torch
+import torch.nn as nn
+import librosa
+
+class LogMelSpectrogram():
+    """Compute the log mel spectrogram of an audio waveform
+    
+        Audio must be sampled at 16,000 sr
+        Audio must also be mono
+    """
+    def __init__(self, mel_filters:torch.Tensor, sample_rate:int, n_samples:int, n_fft:int, hop_length:int):
+        self.mel_floor = 1e-10
+
+        self.mel_filters = mel_filters
+        self.sample_rate = sample_rate
+        self.n_samples = n_samples
+
+        self.spectrogram_transform = T.Spectrogram(n_fft=n_fft,
+                                                   hop_length=hop_length,
+                                                   power=2.0,
+                                                   )
+        self.db_transform = T.AmplitudeToDB(stype="power")
+
+    def convert_to_mel_scale(self, spectrogram):
+        return torch.maximum(torch.Tensor([[self.mel_floor]]), torch.mm(self.mel_filters, spectrogram))
+
+    def pad_or_trim_waveform(self, waveform):
+        waveform_length = waveform.shape[-1]
+        if waveform_length > self.n_samples:
+            waveform = waveform[:-(waveform_length - self.n_samples)]
+        elif waveform_length < self.n_samples:
+            pad = (0, self.n_samples - waveform_length)
+            waveform = nn.functional.pad(waveform, pad, "constant", value=0)
+        return waveform
+   
+    def compute(self, x):
+        x = self.pad_or_trim_waveform(x)
+        x = self.spectrogram_transform(x)
+        x = self.convert_to_mel_scale(x)
+
+        # Got the rest from this function vvvvvv
+        # /home/dylenthomas/miniconda3/envs/python310/lib/python3.10/site-packages/transformers/models/whisper/feature_extraction_whisper.WhisperFeatureExtractor._np_extract_fbank_features
+        # not sure why they did it this way but this makes the spectrogram the same as their feature extractor
+
+        x = torch.abs(x)
+        x = torch.log10(x) # convert to log scale
+        x = torch.maximum(x, torch.max(x) - 8.0)
+        x = (x + 4.0) / 4.0
+        
+        return x
+
+     
+class offlineWhisperProcessor():
+    """Offline processor for whisper decoding and feature extraction
+    """
+    def __init__(self, config_path:str):
+        with open(config_path, 'r') as f:
+            self.config = json.load(f)
+            f.close()
+
+        self.sample_rate = self.config["sampling_rate"]
+        self.hop_length = self.config["hop_length"]
+        self.mel_filters = torch.Tensor(self.config["mel_filters"])
+        self.n_fft = self.config["n_fft"]
+        self.n_samples = self.config["n_samples"]
+        self.nb_max_frames = self.config["nb_max_frames"]
+
+        self.lm_spect_transform = LogMelSpectrogram(self.mel_filters, self.sample_rate, self.n_samples, self.n_fft, self.hop_length)
+    
+    def print_config(self):
+        for key in self.config.keys():
+            print(key)
+            print("\t", self.config[key])
+
+    def extract_features(self, waveform:torch.Tensor):
+        features = self.lm_spect_transform.compute(waveform)
+        if features.shape[-1] > self.nb_max_frames: # sometimes is 3001 and not 3000
+            features = features[:, :-(features.shape[-1] - self.nb_max_frames)]
+        return features.unsqueeze(0)
+    
+
+if __name__ == "__main__":
+    processor = offlineWhisperProcessor("/home/dylenthomas/whisperProject/modelTest/whisper-tiny/preprocessor_config.json")
+
+    waveform, _ = librosa.load("/home/dylenthomas/whisperProject/modelTest/audio/8455-210777-0068.wav", sr=16000, mono=True)
+    waveform = torch.Tensor(waveform)
+
+    waveform_features = processor.extract_features(waveform)
+    print(waveform_features)
--- whisper_main.py
+++ whisper_main.py
@@ -1,20 +1,51 @@
-import torch
-import torchaudio
 from ctypes import * 
 
-class GreedyCTCDecoder(torch.nn.Module):
-    def __init__(self, labels, blank=0):
-        super().__init__()
-        self.labels = labels
-        self.blank = blank
+import tensorflow as tf
+import librosa
+import os
+import numpy as np
 
-    def forward(self, emission: torch.Tensor):
-        indices = torch.argmax(emission, dim=-1) #[num, seq]
-        indices = torch.unique_consecutive(indices, dim=-1)
-        indices = [i for i in indices if i != self.blank]
-        return "".join([self.labels[i] for i in indices])
+ASR_dir = "/home/dylenthomas/whisperProject/ASR_TFLite" 
+model_path = f"{ASR_dir}/pre_trained_models/English/subword-conformer.latest_for_english.tflite"
+audio_path = f"{ASR_dir}/wavs/1089-134691-0000.flac"
 
-labels = ('-', '|', 'E', 'T', 'A', 'O', 'N', 'I', 'H', 'S', 'R', 'D', 'L', 'U', 'M', 'W', 'C', 'F', 'G', 'Y', 'P', 'B', 'V', 'K', "'", 'X', 'J', 'Q', 'Z')
+audio_data, _ = librosa.load(os.path.expanduser(audio), sr=16000, mono=True)
+interpreter = tf.lite.Interpreter(model_path=model_path)
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+interpreter.resize_tensor_input(input_details[0]["index"], signal.shape)
+interpreter.allocate_tensors()
+interpreter.set_tensor(input_details[0]["index"], signal)
+interpreter.set_tensor(
+    input_details[1]["index"],
+    np.array(0).astype("int32")
+)
+interpreter.set_tensor(
+    input_details[2]["index"],
+    np.zeros([1,2,1,320]).astype("float32")
+)
+interpreter.invoke()
+pred = interpreter.get_tensor(output_details[0]["index"])
+
+print("".join([chr(u) for u in pred]))
+
+exit()
+#class GreedyCTCDecoder(torch.nn.Module):
+#    def __init__(self, labels, blank=0):
+#        super().__init__()
+#        self.labels = labels
+#        self.blank = blank
+#
+#    def forward(self, emission: torch.Tensor):
+#        indices = torch.argmax(emission, dim=-1) #[num, seq]
+#        indices = torch.unique_consecutive(indices, dim=-1)
+#        indices = [i for i in indices if i != self.blank]
+#        return "".join([self.labels[i] for i in indices])
+#
+#labels = ('-', '|', 'E', 'T', 'A', 'O', 'N', 'I', 'H', 'S', 'R', 'D', 'L', 'U', 'M', 'W', 'C', 'F', 'G', 'Y', 'P', 'B', 'V', 'K', "'", 'X', 'J', 'Q', 'Z')
 
 clib = CDLL("/home/dylenthomas/whisper/whispermodule.so")
 
@@ -30,13 +61,12 @@ ptr = clib.accessMicrophone(b"microphone name", 16000, 1, 512, 5, byref(sample_c
 mic_samples = [ptr[i] for i in range(sample_count.value)]
 clib.freeBuffer(ptr)
 
-Wav2Vec2 = torch.jit.load("/home/dylenthomas/whisper/.model/Wav2Vec2.pt")
-decoder = GreedyCTCDecoder(labels)
+#Wav2Vec2 = torch.jit.load("/home/dylenthomas/whisper/.model/Wav2Vec2.pt")
+#decoder = GreedyCTCDecoder(labels)
 
+#mic_samples = torch.tensor(mic_samples)
+#with torch.inference_mode():
+#    emission, _ = Wav2Vec2(mic_samples)
 
-mic_samples = torch.tensor(mic_samples)
-with torch.inference_mode():
-    emission, _ = Wav2Vec2(mic_samples)
-
-transcript = decoder(emission[0])
-print(transcript)
+#transcript = decoder(emission[0])
+#print(transcript)
