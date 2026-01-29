149437dbb7b36dc2c365d1ea88139d244c2866c7
Dylen Liverman <dliverman621@gmail.com>
2026-01-28 22:36:50 -0500

    timed onnx vad, and its too long

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
 
 #include "onnxruntime_c_api.h"
 #include "mic_access.h"
@@ -16,32 +15,62 @@ int badStatus(OrtStatus* status, const OrtApi* ort) {
 	return 0;
 }
 
-double timedifference_msec(struct timeval t0, struct timeval t1) {
-    return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
+float getSpeechProb(
+		const OrtApi* ort,
+		OrtSession* session,
+		OrtRunOptions* run_ops,
+		OrtValue* input_tensor,
+		OrtValue* state_tensor,
+		OrtValue* sr_tensor
+		) {
+	static const char* input_names[] = {"input", "state", "sr"};
+	static const char* output_names[] = {"output", "stateN"};
+		
+	const OrtValue* const inputs[3] = {input_tensor, state_tensor, sr_tensor};
+	OrtValue* outputs[2] = {NULL, NULL};
+	
+	if (badStatus(ort->Run(
+		session, run_opts, input_names, inputs, 3,
+		output_names, 2, outputs), ort)) { return 1; }
+
+	// Retrieve probability of speech from the model
+	OrtValue* ort_speech_prob = outputs[0];
+	
+	float* prob_data = NULL;
+	if (badStatus(ort->GetTensorMutableData(ort_speech_prob, (void**)&prob_data), ort)) { return 1; }
+	float speech_prob = prob_data[0];
+
+	// release old OrtValues
+	// TODO check if I need to release input_tensor
+	ort->ReleaseValue(state_tensor);
+	ort->ReleaseValue(outputs[0]);
+	
+	// Save the previous state for the next run.
+	state_tensor = outputs[1];
+	outputs[1] = NULL;
+
+	return speech_prob;
 }
 
+// TODO implement auto read onnx model parameters
 int main(int argc, char *argv[]) {
 	if (argc != 3) { printf("There should be only two args, VAD model path then mic name.\n"); return 0; }
 	const char* vad_model_path = argv[1];
 	const char* mic1_name = argv[2];
 
-    struct timeval t0, t1;
+	const float speech_threshold = 0.7; // trigger threshold to start transcription
 
-	int64_t sample_rate[] = {16000};
+	const int64_t sample_rate[] = {16000};
 	const int64_t sample_rate_shape[] = {1};
 
-	int64_t input_data_shape[] = {1, 512}; // input data will be the mic buffer
-	int64_t state_data_shape[] = {2, 1, 128};	
+	const int64_t input_data_shape[] = {1, 512}; // input data will be the mic buffer
+	const int64_t state_data_shape[] = {2, 1, 128};	
 
 	int16_t tmp_buffer[512] = {0};
+    int16_t long_buffer[16000 * 30] = {0}; // 30 seconds is the most audio transcript model can take
 	float buffer[512] = {0};
 	float initial_state[2 * 1 * 128] = {0};
 
-	const char* input_names[] = {"input", "state", "sr"};
-	//const char* const* input_names = &input_names_arr;
-	const char* output_names[] = {"output", "stateN"};
-	//const char* const* output_names = &output_names_arr;
-	
 	OrtValue* input_tensor = NULL;
 	OrtValue* state_tensor = NULL;
 	OrtValue* sr_tensor = NULL;
@@ -55,12 +84,12 @@ int main(int argc, char *argv[]) {
 
 	OrtSessionOptions* session_opts = NULL;
 	if (badStatus(ort->CreateSessionOptions(&session_opts), ort)) { return 1; }
-	if (badStatus(ort->SetIntraOpNumThreads(session_opts, 5), ort)) { return 1; }
-	if (badStatus(ort->SetInterOpNumThreads(session_opts, 5), ort)) { return 1; }
+	if (badStatus(ort->SetIntraOpNumThreads(session_opts, 1), ort)) { return 1; }
+	if (badStatus(ort->SetInterOpNumThreads(session_opts, 1), ort)) { return 1; }
 	if (badStatus(ort->SetSessionGraphOptimizationLevel(session_opts, ORT_ENABLE_ALL), ort)) { return 1; }
 
-	OrtSession* session = NULL;
-	if (badStatus(ort->CreateSession(env, vad_model_path, session_opts, &session), ort)) { return 1; }
+	OrtSession* ort_session = NULL;
+	if (badStatus(ort->CreateSession(env, vad_model_path, session_opts, &ort_session), ort)) { return 1; }
 
 	OrtRunOptions* ort_run_opts = NULL;
 	if (badStatus(ort->CreateRunOptions(&ort_run_opts), ort)) { return 1; }
@@ -83,51 +112,37 @@ int main(int argc, char *argv[]) {
 	if (badStatus(ort->CreateTensorWithDataAsOrtValue(
 		mem_info, buffer, sizeof(buffer), input_data_shape, 2, 
 		ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, &input_tensor), ort)) { return 1; }
-	
 // -------------------------------------------------------------------------------------------------
 // Initialize Microphone ---------------------------------------------------------------------------
 	printf("Initializing microphone...\n");
 	snd_pcm_t* mic1_ch;
-	
 	init_mic(mic1_name, &mic1_ch, sample_rate[0], 1, 512);
 // -------------------------------------------------------------------------------------------------
 	printf("Starting audio collection.\n");
-	//uint64_t start = get_current_time_ms();
 	while (1) {
-        gettimeofday(&t0, NULL); 
-		OrtValue* outputs[2] = {NULL, NULL};
+		float speech_prob;
+		
 		read_mic(tmp_buffer, mic1_ch, 512); // read 512 buffer samples
-
 		int i = 0;
-		while (i < 512) { buffer[i] = (float)tmp_buffer[i] / 32768.0f; i++; }
-
-		const OrtValue* const inputs[3] = {input_tensor, state_tensor, sr_tensor};
-
-		if (badStatus(ort->Run(
-			session, ort_run_opts, input_names, inputs, 3,
-			output_names, 2, outputs), ort)) { return 1; }
-
-		// Retrieve probability of speech from the model
-		OrtValue* ort_speech_prob = outputs[0];
-		float* prob_data = NULL;
-		if (badStatus(ort->GetTensorMutableData(ort_speech_prob, (void**)&prob_data), ort)) { return 1; }
-		float speech_prob = prob_data[0];
-
-		// release old OrtValues
-		ort->ReleaseValue(state_tensor);
-		ort->ReleaseValue(outputs[0]);
-		// Save the previous state for the next run.
-		state_tensor = outputs[1];
-		outputs[1] = NULL;
-
-        gettimeofday(&t1, NULL);
-		printf("\033[2J\033[H");
-		printf("[%f] The probability of speech is: %f\n", timedifference_msec(t0, t1) * 1000, speech_prob); 	
-    }
+		while (i < 512) { buffer[i] = (float)tmp_buffer[i] / 32768.0f; i++; } // convert int mic data to float
+		
+        int z = 0;
+		speech_prob = getSpeechProb(ort, ort_session, ort_run_ops, input_tensor, state_tensor, sr_tensor);
+		while (speech_prob >= speech_threshold) {
+			// collect audio data for transcription
+		    read_mic(tmp_buffer, mic1_ch, 512); // read 512 buffer samples
+            while(x < 512) { long_buffer[x + 512 * z] = tmp_buffer[x]; x++;}
+			
+            speech_prob = getSpeechProb(ort, ort_session, ort_run_ops, input_tensor, state_tensor, sr_tensor);
+            z++;
+		}
+        // should keep track of z because z = the number of buffers read, so we know where to 
+        // stop transcribing in the long buffer
+	}
 	
 // Cleanup for program exit ------------------------------------------------------------------------
 	ort->ReleaseMemoryInfo(mem_info);
-	ort->ReleaseSession(session);
+	ort->ReleaseSession(ort_session);
 	ort->ReleaseSessionOptions(session_opts);
 	ort->ReleaseEnv(env);
 
