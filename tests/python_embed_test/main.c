96d950e78bbca6d1dde9abb443a2365b1ff366dc
Dylen Liverman <dliverman621@gmail.com>
2026-04-24 12:58:51 +0000

    Changed to use PyConfig to set the sys.path variable

--- tests/python_embed_test/main.c
+++ tests/python_embed_test/main.c
@@ -1,15 +1,18 @@
 #include <Python.h>
-#include <wchar.h>
 
 int main(int argc, char* argv[]) {
-    Py_Initialize();
+    PyStatus status;
+    PyConfig config;
 
-    PyObject *sys_path = PySys_GetObject("path");
-    PyObject *test_path_str = PyUnicode_FromString("/home/dylenthomas/LiveASRonRPi-4/tests/python_embed_test");
-    PyObject *site_packages_path_str = PyUnicode_FromString("/home/dylenthomas/LiveASRonRPi-4/tests/python_embed_test/build/venv/lib/python3.14/site-packages");
-    PyList_Append(sys_path, test_path_str);
-    PyList_Append(sys_path, site_packages_path_str);
+    PyConfig_InitPythonConfig(&config);
 
+    // It would be proper to error check these status values
+    // To more robustly get these paths just go up from the CWD and look for "venv" in the CWD
+    status = PyConfig_SetString(&config, &config.pythonpath_env, L"/home/dylenthomas/LiveASRonRPi-4/tests/python_embed_test:/home/dylenthomas/LiveASRonRPi-4/tests/python_embed_test/build/venv/lib/python3.14/site-packages");
+    status = Py_InitializeFromConfig(&config);
+
+    PyConfig_Clear(&config);
+   
     PyRun_SimpleString("import sys; print('sys.path:', sys.path)");
 
     PyObject* pModule = PyImport_Import(PyUnicode_DecodeFSDefault("pythonTest"));
@@ -23,8 +26,6 @@ int main(int argc, char* argv[]) {
     PyObject *pResult = PyObject_CallNoArgs(pFunc);
 
     Py_DECREF(pModule);
-    Py_DECREF(test_path_str);
-    Py_DECREF(site_packages_path_str);
     Py_DECREF(pFunc);
     Py_DECREF(pResult);
 }
--- tests/python_embed_test/pythonTest.py
+++ tests/python_embed_test/pythonTest.py
@@ -1,6 +1,7 @@
 import numpy as np
+import torch
 
 def main(): 
-    print("Hello, this is a numpy test..")
+    print("Hello, this is a numpy and pytorch test..")
     arr = np.array([1, 2, 3, 4, 5])
     print(arr)
